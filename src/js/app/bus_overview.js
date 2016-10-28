c172368d9d7ee562ff9fe44af4a42e6fbc771e30
jessie <junchangchen@gmail.com>
2016-10-28 15:15:12 +0800

    refactor

--- src/js/app/bus_overview.js
+++ src/js/app/bus_overview.js
@@ -8,46 +8,6 @@ if (process.env.NODE_ENV !== 'production') {
     // }
 }
 
-/*
-
-import "../../styles/bus_overview.scss";
-import configureStore from '../store/configureStore';
-import {ready} from '../utils';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, hashHistory } from 'react-router';
-import { Provider } from 'react-redux';
-import BusOverview from '../containers/BusOverview';
-
-
-const initialState = {};
-const store = configureStore(initialState);
-
-const routes = (
-    <Route path="/" component={BusOverview}>
-    </Route>
-);
-
-
-function main() {
-    ReactDOM.render(
-        <Provider store={store}>
-            <Router history={hashHistory} routes={routes}/>
-        </Provider>,
-
-        document.getElementById("root")
-    )
-}
-
-
-ready(function () {
-    main();
-});
-
-*/
-
-
-
 import "../../styles/bus_overview.scss";
 import React from 'react';
 import { Route } from 'react-router';
